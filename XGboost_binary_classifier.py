2b6be9303838e4c4821fc1b9c02dd1c5e00aad23
Belabbaci A.Nazim <30374106+NazimBL@users.noreply.github.com>
2022-05-20 15:16:50 -0500

    Update and rename main.py to XGboost_binary_classifier.py

--- XGboost_binary_classifier.py
+++ XGboost_binary_classifier.py
@@ -6,23 +6,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from numpy import loadtxt
 
-"""""
-
-df = pd.read_csv("cfdna_dataset.csv")
-del df["Patient"]
-del df["Timepoint"]
-df = df.replace(["Y","N"],[1,0])
-#df['Patient Type'] = 1
-
-df = df.replace(["Healthy"],[0])
-df = df.replace(["Breast Cancer", "Cholangiocarcinoma", "Colorectal Cancer"
-                , "Gastric cancer", "Lung Cancer", "Ovarian Cancer"
-                ,  "Pancreatic Cancer", "Bile Duct Cancer", "Gastric Cancer"],[1,1,1,1,1,1,1,1,1])
-
-df['Percent Mapped to Target Regions'] = df['Percent Mapped to Target Regions'].str.rstrip('%').astype('float') / 100.0
-df.to_csv("data.csv", index=False)
-print(df)
-"""
 dataset = loadtxt('data.csv', delimiter=",")
 # split data into X and y
 X = dataset[:,1:10]
