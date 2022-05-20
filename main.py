8299b7e44ce15c3d1788208ddbea01cfda5ae84c
Belabbaci A.Nazim <30374106+NazimBL@users.noreply.github.com>
2022-05-20 14:56:46 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+
+from xgboost import XGBClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from numpy import loadtxt
+
+"""""
+df = pd.read_csv("cfdna_dataset.csv")
+del df["Patient"]
+del df["Timepoint"]
+df = df.replace(["Y","N"],[1,0])
+#df['Patient Type'] = 1
+
+df = df.replace(["Healthy"],[0])
+df = df.replace(["Breast Cancer", "Cholangiocarcinoma", "Colorectal Cancer"
+                , "Gastric cancer", "Lung Cancer", "Ovarian Cancer"
+                ,  "Pancreatic Cancer"],[1,1,1,1,1,1,1])
+
+df['Percent Mapped to Target Regions'] = df['Percent Mapped to Target Regions'].str.rstrip('%').astype('float') / 100.0
+df.to_csv("data.csv", index=False)
+print(df)
+"""""
+
+#dataset = loadtxt('data.csv', delimiter=",")
+#print(dataset)
